866059369780eb231ea72026d18df40fcc9813c8
Satya Kolipaka <satyasudheer625@gmail.com>
2021-12-03 09:39:33 +0100

    Intitial project with required packages

---
+++ src/main/java/com/heycar/listing/ListingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.heycar.listing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListingServiceApplication.class, args);
+	}
+
+}
